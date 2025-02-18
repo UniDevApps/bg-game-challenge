3a2b681e9116317f247b331682cfaf10ebda5b6c
Leon Farafonov <117378906+leofaraf@users.noreply.github.com>
2025-02-18 11:45:32 +0300

    feat: colordash game!

--- src/routes/colordash/game/+page.svelte
+++ src/routes/colordash/game/+page.svelte
@@ -2,11 +2,14 @@
     import { afterNavigate, goto } from "$app/navigation";
     import { onDestroy, onMount } from "svelte";
     import { writable } from "svelte/store";
+    import { walletCredit } from "../../store";
 
     let canvas: HTMLCanvasElement
     let context: CanvasRenderingContext2D | null
 
     let isRunning = writable(location.search != "?gameover")
+
+    let earned = writable(0)
 
     const background = {
         y: 0,
@@ -15,19 +18,24 @@
     };
     background.image.src = '/backgrounds/colordash.png'; 
 
+    const generateId = () => Math.floor(Math.random() * 3) + 1;
+
     const ball = {
         dx: () => canvas.width / 2 - ball.radius / 2,
         dy: () => canvas.height - ball.radius - 150,
         speed: 5,
         radius: 100,
-        image: new Image()
+        image: new Image(),
+        id: generateId(),
     };
-    ball.image.src = `/sprites/colordash/ball_${Math.floor(Math.random() * 3) + 1}.png`;
+    ball.image.src = `/sprites/colordash/ball_${ball.id}.png`;
 
     interface Wall {
         x: number,
         y: number,
-        image: HTMLImageElement
+        id: number,
+        image: HTMLImageElement,
+        paid: boolean,
     }
 
     interface Walls {
@@ -71,11 +79,24 @@
         walls.items.forEach((item, index) => {
             if (context != null) {
                 context.drawImage(item.image, item.x, item.y, walls.width(), walls.height);
-                item.y += walls.speed;
+                item.y += $isRunning ? walls.speed : 0;
 
-                // Remove money if it goes off screen
-                if (item.y > canvas.height) {
-                    walls.items.splice(index, 1);
+                if (item.y > ball.dy()) {
+                    if (item.paid) {
+                        // Remove money if it goes off screen
+                        if (item.y > canvas.height) {
+                            walls.items.splice(index, 1);
+                        }
+                    } else {
+                        item.paid = true
+                        if (item.id == ball.id) {
+                            walletCredit(10)
+                            $earned += 10
+                        } else {
+                            $isRunning = false
+                        }
+                        console.log("Handle")
+                    }
                 }
             }
         })
@@ -113,11 +134,14 @@
 
     function createWall() {
         const moneyImage = new Image();
-        moneyImage.src = `/sprites/colordash/wall_${Math.floor(Math.random() * 3) + 1}.png`; // Replace with your money image
+        let id = generateId()
+        moneyImage.src = `/sprites/colordash/wall_${id}.png`; // Replace with your money image
         walls.items.push({
             x: 0,
             y: 0-walls.height,
-            image: moneyImage
+            image: moneyImage,
+            paid: false,
+            id: id
         });
     }
 
@@ -147,6 +171,8 @@
 
     const reloadButton = () => {
         $isRunning = true
+        $earned = 0
+        walls.items = []
         goto("/colordash/game")
     }
 
@@ -157,15 +183,20 @@
     const resize = () => {
         console.log("resize")
     }
+
+    const changeBall = (id: number) => {
+        ball.id = id
+        ball.image.src = `/sprites/colordash/ball_${ball.id}.png`;
+    }
 </script>
 
 {#if $isRunning != true}
     <div class="gameover">
         <h1 class="red-text">Game Over</h1>
-        <h2>You earned <span class="grn-text">100 coins!</span></h2>
+        <h2>You earned <span class="grn-text">{$earned} coins!</span></h2>
         <div class="gameover-btns">
             <button onclick={reloadButton}>
-                <img src="/items/reload_button.png" alt="">
+                <img class="gameover-img" src="/items/reload_button.png" alt="">
             </button>
             <button onclick={exitButton}>
                 <img src="/items/exit_button.png" alt="">
@@ -173,9 +204,24 @@
         </div>
     </div>
 {/if}
+<div class="choose-menu">
+    <button class="choose-btn" onclick={() => changeBall(1)}>
+        <img class="choose-img" src="/sprites/colordash/btn_1.svg" alt="btn">
+    </button>
+    <button class="choose-btn" onclick={() => changeBall(2)}>
+        <img class="choose-img" src="/sprites/colordash/btn_2.svg" alt="btn">
+    </button>
+    <button class="choose-btn" onclick={() => changeBall(3)}>
+        <img class="choose-img" src="/sprites/colordash/btn_3.svg" alt="btn">
+    </button>
+    <button class="choose-btn" onclick={() => changeBall(4)}>
+        <img class="choose-img" src="/sprites/colordash/btn_4.svg" alt="btn">
+    </button>
+</div>
 <canvas
     bind:this={canvas}
-    onresize={resize}></canvas>
+    onresize={resize}
+></canvas>
 
 <style>
     canvas {
@@ -195,6 +241,7 @@
 
     .gameover {
         position: absolute;
+        z-index: 2;
         background-color: rgba(0, 0, 0, 0.5);
         width: 100vw;
         height: 100vh;
@@ -209,5 +256,27 @@
         display: flex;
         gap: 50px;
         margin: 20px;
+    }
+
+    .choose-menu {
+        position: absolute;
+        z-index: 1;
+        bottom: 0;
+        width: 100%;
+        padding-bottom: 40px;
+    }
+
+    .choose-menu {
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        width: 100vw;
+    }
+
+    .choose-btn {
+        width: 100%;
+    }
+
+    .choose-img {
+        width: 100%;
     }
 </style>
