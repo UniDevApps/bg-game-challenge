660dabf30d3c0ba689f758bf1f11259e97076571
Leon Farafonov <117378906+leofaraf@users.noreply.github.com>
2025-02-20 11:42:10 +0300

    feat: transparent status bar and new version

--- src-tauri/gen/android/app/src/main/java/play/bcgame/bounce/challenge/MainActivity.kt
+++ src-tauri/gen/android/app/src/main/java/play/bcgame/bounce/challenge/MainActivity.kt
@@ -1,3 +1,18 @@
 package play.bcgame.bounce.challenge
 
-class MainActivity : TauriActivity()
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    // Allow content to extend under the system bars
+    WindowCompat.setDecorFitsSystemWindows(window, false)
+
+    // Get the insets controller to manage system UI
+    val windowInsetsController = WindowCompat.getInsetsController(window, window.decorView)
+
+
+    // Ensure the status bar is fully transparent
+    window.statusBarColor = android.graphics.Color.TRANSPARENT  }
+}
